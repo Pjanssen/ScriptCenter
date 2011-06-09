ae18feca3e5478597686f3212b21e09eb5208b03
Pier Janssen <pier@threesixty.nl>
2011-06-09 13:24:00 +0000

    Added TextBoxStreamWriter extending TextWriter, to direct InstallerLog to TextBox control.

---
+++ ScriptCenter/Controls/TextBoxStreamWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ScriptCenter.Controls
+{
+    public class TextBoxStreamWriter : TextWriter
+    {
+        private TextBox _textBox;
+
+        public TextBoxStreamWriter(TextBox textBox)
+        {
+            _textBox = textBox;
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+
+        public override void Write(char value)
+        {
+            _textBox.AppendText(value.ToString());
+
+            //For future reference (for cross-thread calls):
+            //MethodInvoker action = delegate { _textBox.AppendText(value.ToString()); };
+            // _textBox.BeginInvoke(action);
+        }
+
+        public override void Write(string value)
+        {
+            _textBox.AppendText(value);
+        }
+    }
+}
---
+++ ScriptCenterTest/Controls/TextBoxStreamWriterTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScriptCenter.Controls;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ScriptCenterTest.Controls
+{
+    [TestClass]
+    public class TextBoxStreamWriterTest
+    {
+        TextWriter consoleStdOut;
+        TextBox textBox;
+        TextBoxStreamWriter writer;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            textBox = new TextBox();
+            writer = new TextBoxStreamWriter(textBox);
+            consoleStdOut = Console.Out;
+            Console.SetOut(writer);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Console.SetOut(consoleStdOut);
+        }
+
+        [TestMethod]
+        public void EncodingTest()
+        {
+            Assert.AreEqual(System.Text.Encoding.UTF8, writer.Encoding, "Encoding should be UTF8");
+        }
+
+        [TestMethod]
+        public void WriteTest()
+        {
+            Assert.AreEqual("", textBox.Text, "Initial textbox text");
+            String line = "test";
+            Console.Out.Write(line);
+            Assert.AreEqual(line, textBox.Text, "Textbox text after Write to console.");
+
+            textBox.Text = String.Empty;
+            Assert.AreEqual(String.Empty, textBox.Text, "Reset textBox text.");
+
+            Console.Out.WriteLine(line);
+            Assert.AreEqual(line + Environment.NewLine, textBox.Text, "Textbox text after WriteLine to console.");
+        }
+    }
+}
