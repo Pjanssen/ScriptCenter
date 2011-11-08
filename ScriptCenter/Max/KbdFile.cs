47f2f7efbc1227aabed851ff2e7ccbcb143268a0
Pier Janssen <pier@threesixty.nl>
2011-11-08 18:36:53 +0000

    Minor bugfixes in KbdFile (Added Read() to KbdFile(String) ctor, changed Write from OpenOrCreate to Create)

--- ScriptCenter/Max/KbdFile.cs
+++ ScriptCenter/Max/KbdFile.cs
@@ -1,20 +1,4 @@
-﻿//  Copyright 2011 P.J. Janssen
-//  This file is part of ScriptCenter.
-
-//  ScriptCenter is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-
-//  ScriptCenter is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-
-//  You should have received a copy of the GNU General Public License
-//  along with ScriptCenter.  If not, see <http://www.gnu.org/licenses/>.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,6 +31,7 @@ internal class KbdFile
     public KbdFile(String file) : this()
     {
         this.File = file;
+        this.Read();
     }
 
     public const Int32 MainTableId = 0;
@@ -285,7 +270,7 @@ internal class KbdFile
             if (System.IO.File.Exists(this.File))
                 System.IO.File.Copy(this.File, this.File + ".bak", true);
 
-            using (FileStream stream = new FileStream(this.File, FileMode.OpenOrCreate))
+            using (FileStream stream = new FileStream(this.File, FileMode.Create))
             {
                 return this.Write(stream);
             }
