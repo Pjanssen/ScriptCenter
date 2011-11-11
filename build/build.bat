f67bb05bae0ba43d8181301928d8e01b23c2561e
Pier Janssen <pier@threesixty.nl>
2011-11-12 00:44:47 +0100

    Added build.bat

---
+++ build/build.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+
+::Adjust the value of MaxDir to match your local 3dsmax installation directory.
+SET MaxDir=C:\Program Files\Autodesk\3ds Max 2010\
+
+SET MSBuild=%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe
+
+
+ECHO Building ScriptCenter...
+SET Configuration=Release
+%MSBuild% ..\ScriptCenter\ScriptCenter.csproj /nologo /p:ReferencePath="%MaxDir%;" || goto :error
+
+
+ECHO.
+ECHO Deploying compiled files...
+IF EXIST deploy ( rmdir deploy /S /Q || goto :error )
+mkdir deploy || goto :error
+copy ..\ScriptCenter\bin\%Configuration%\*.dll deploy\ || goto :error
+
+
+ECHO.
+ECHO Done.
+goto :eof
+
+
+:error
+PAUSE
+EXIT /B %ERRORLEVEL%
+
+
+::C:\Program Files\Autodesk\3ds Max 2010\
